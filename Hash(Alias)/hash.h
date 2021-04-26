dc47d8dd67e7280ac9bc342446f55bd3382620d3
srikardesu <83106928+srikardesu@users.noreply.github.com>
2021-04-26 14:49:13 +0530

    Create hash.h
    Member functions for the hash(Alias) part.

---
+++ Hash(Alias)/hash.h
@@ -0,0 +1,18 @@
+#ifndef _HASHTABLE_H_
+
+#define _HASHTABLE_H
+
+typedef long long Element;
+typedef long long Key;
+typedef struct stHT_alias *Alias;
+
+#define _invalid -5555;
+
+
+//mini proj
+
+void Insert_path_qp(char arr1[50], char arr2[20], struct alias qp[], long long size);
+struct stHT_alias *createhash(int size);
+int hornercalc_modiiit(char arr[20], int size);
+
+//////////////////////////////////////////////////////////////////////////////////////
