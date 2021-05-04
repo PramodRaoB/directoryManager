78a221ef6beb7b808845ad10992ac4e69eb905bb
PramodRaoB <pramod.b@research.iiit.ac.in>
2021-05-05 03:12:28 +0530

    Redifine node element to account for major restructuring

--- Directree/NodeElement.h
+++ Directree/NodeElement.h
@@ -1,20 +1,24 @@
 #ifndef NODE_ELEMENT_H
 #define NODE_ELEMENT_H
 
-#define NAME_SIZE 5
+#define NAME_SIZE 64
+
 #include <stdbool.h>
 
 typedef struct treenode TreeNode;
+typedef struct filedetails FileDetails;
 
 struct treenode {
-  char name[NAME_SIZE];
-  bool is_file;
-  unsigned int hash;
+  FileDetails *file;
 
-  TreeNode *firstchild;
+  TreeNode *first_child;
   TreeNode *next;
   TreeNode *current;
   TreeNode *parent;
 };
 
+struct filedetails {
+  char *name;
+  bool is_file;
+};
 #endif
