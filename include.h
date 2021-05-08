c243744cf2bd62fda8a3596c0404928b2580e9dd
BuzzingTaz <tejah.ss@research.iiit.ac.in>
2021-05-08 13:06:13 +0530

    Added include.h

---
+++ include.h
@@ -0,0 +1,20 @@
+#ifndef __INCLUDE_H__
+#define __INCLUDE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "Directree/tree.h"
+#include "Directree/hashChild.h"
+#include "Directree/NodeElement.h"
+
+#include "Hash(Alias)/hash.h"
+
+#include "utils/string_parser.h"
+#include "utils/utils.h"
+
+
+#endif
