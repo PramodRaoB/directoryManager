eaa1a464e078935c638783bde99f12c4fa656a75
varshitakolipaka <varshita2003@gmail.com>
2021-05-09 18:00:53 +0400

    minor tweaks to help func

--- DirectoryManager.c
+++ DirectoryManager.c
@@ -92,8 +92,7 @@ void FIND(TreeNode *currentDir) {
   //printf("Enter prefix string: ");
 
   char *prefixStr = read_string();
-  if(prefixStr[0]=='\0')
-  {
+  if (prefixStr[0] == '\0') {
     printf("Error: No prefix string found\n");
     return ;
   }
@@ -107,19 +106,19 @@ void FIND(TreeNode *currentDir) {
 void HELP() {
   printf("\nAvailable commands:\n\n");
   printf("add\t\tAdd file/directory inside current directory\n");
-  printf("\t\tadd _name _type\n\n");
+  printf("\t\tadd <name> <type>\n\n");
   printf("move\t\tChange current directory to any directory by taking input the complete path to that directory\n");
-  printf("\t\tmove _path\n\n");
+  printf("\t\tmove <path>\n\n");
   printf("alias\t\tTakes input the complete path to that directory and the alias\n");
-  printf("\t\talias _path _aliasforpath\n\n");
+  printf("\t\talias <path> <aliasforpath>\n\n");
   printf("teleport\tChange the current directory to any directory by taking input the alias of that directory\n");
-  printf("\t\tteleport _aliasforpath\n\n");
+  printf("\t\tteleport <aliasforpath>\n\n");
   printf("find\t\tSearch for all the files/directories with given prefix inside current directory\n");
-  printf("\t\tfind _prefix\n\n");
+  printf("\t\tfind <prefix>\n\n");
   printf("ls\t\tPrints all the files/folders in the current directory\n");
   printf("\t\tls\n\n");
   printf("cd\t\tChange current directory to any directory inside the current directory by taking input the relative path to that directory from the\n\t\tcurrent directory\n");
-  printf("\t\tcd _path\n\n");
+  printf("\t\tcd <path>\n\n");
   printf("quit\t\tTerminates the program\n");
   printf("\t\tquit\n\n");
 }
--- doc.md
+++ doc.md
@@ -3,4 +3,16 @@ To setup the program on your desktop, do the following. `Insert dependencies if 
 `insert some commands`
 # Usage
 `Insert makefile and commands to run program`
+# Features
+1. Add a file in the current directory using the `add` function
+2. Move to any directory using `move`.
+3. Save path to directory using `alias`.
+4. Move to any directory using `teleport`by providing alias of the path.
+5. `find`allows you to search through the current directory with a specified prefix
+6. Exit the program using `quit`
+7. Display contents of current directory using `ls`.
+8. Use `cd` to move to directory of choice from the current directory.
+9. Use `help` to learn more about the usage.
+
+
 
