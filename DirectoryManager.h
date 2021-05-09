7559b60b3c27748785705a3fe5855cadaa55bf29
BuzzingTaz <tejah.ss@research.iiit.ac.in>
2021-05-09 20:06:18 +0530

    added comments

--- DirectoryManager.h
+++ DirectoryManager.h
@@ -5,6 +5,7 @@
 
 /*
  * Prints a welcome message
+ * and a basic description of the program
  * */
 void STARTUP();
 /*
@@ -29,12 +30,30 @@ TreeNode *MOVE(TreeNode *root, TreeNode *current);
  * */
 AliasTableStruct ALIAS(AliasTableStruct table, TreeNode *root);
 
+/*
+ * TELEPORT function takes in the Alias table and root of the tree.
+ * It scans for the alias and moves the root to the location
+ * pointed by the alias
+ * */
 TreeNode *TELEPORT(AliasTableStruct table, TreeNode *root);
 
+/*
+ * Takes the pointer to current directory
+ * Takes user input for the prefix to be searched
+ * Uses a trie data structure to print all files in the current directory
+ * starting with the prefix
+ * */
 void FIND(TreeNode *currentDir);
 
+/*
+ * Prints a help message containing a short description of the cli commands,
+ * their usage and examples
+ * */
 void HELP();
 
+/*
+ * Frees all remaining heap blocks and exits the program
+ * */
 void QUIT(TreeNode *root, AliasTableStruct alias_table);
 
 #endif
